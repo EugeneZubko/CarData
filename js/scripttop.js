fb914131d79de63b42d587abcc43ad2f3f75f80d
Eugene <101331706+EugeneZubko@users.noreply.github.com>
2022-03-25 22:12:35 +0300

    Add files via upload

---
+++ js/scripttop.js
@@ -0,0 +1,23 @@
+$(function(){
+  $.fn.scrollToTop=function(){
+    $(this).hide().removeAttr("href");
+    if($(window).scrollTop()!="0"){
+        $(this).fadeIn("slow")
+  }
+  var scrollDiv=$(this);
+  $(window).scroll(function(){
+    if($(window).scrollTop()=="0"){
+    $(scrollDiv).fadeOut("slow")
+    }else{
+    $(scrollDiv).fadeIn("slow")
+  }
+  });
+    $(this).click(function(){
+      $("html, body").animate({scrollTop:0},"slow")
+    })
+  }
+});
+$(function() {$("#up1").scrollToTop();});
+$(function() {$("#up").scrollToTop();});
+$(function() {$("#toTop").scrollToTop();});
+// $(function() {$(".thumbs").scrollToTop();});
